c87c578208943b529f3376d5ca44c9b57b829130
Ruslan_Novik1 <Ruslan_Novik1@epam.com>
2021-12-10 23:07:35 +0300

    fix lint 5

--- src/common/config.ts
+++ src/common/config.ts
@@ -9,4 +9,3 @@ export const { PORT } = process.env;
 export const { NODE_ENV } = process.env;
 export const { MONGO_CONNECTION_STRING } = process.env;
 export const { JWT_SECRET_KEY } = process.env;
-export const AUTH_MODE = process.env.AUTH_MODE === 'true';

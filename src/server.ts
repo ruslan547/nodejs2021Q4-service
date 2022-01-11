e4bda4e0442109d3945d1c2abe4718581c3c67c0
Ruslan_Novik1 <Ruslan_Novik1@epam.com>
2022-01-12 00:15:24 +0300

    add typeorm

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
+import { createConnection } from 'typeorm';
 import { PORT } from './common/config';
 import app from './app';
 
-app.listen(PORT, () => console.log(`App is running on http://localhost:${PORT}`));
+createConnection().then(() => {
+  app.listen(PORT, () => console.log(`App is running on http://localhost:${PORT}`));
+}).catch((error: Error) => console.log(error));
